47db7dc5bf4a6ac32d6476e5ab04a5afd7030e60
YanfuOu <122131796+YanfuOu@users.noreply.github.com>
2024-04-30 23:23:39 -0400

    Update UART_Reading.ino

--- ESP32MotorControl/final_project/UART_Reading/UART_Reading.ino
+++ ESP32MotorControl/final_project/UART_Reading/UART_Reading.ino
@@ -1,14 +1,14 @@
 #include <HardwareSerial.h>
 
 HardwareSerial MySerial(2); // define a Serial for UART1
-const int MySerialRX = 16;
-const int MySerialTX = 17;
+const int RX = 16;
+const int TX = 17;
 
 void setup()
 {
 // initialize the Serial to the pins
 Serial.begin(9600);
-MySerial.begin(9600, SERIAL_8N1, MySerialRX, MySerialTX);
+SerialTWO.begin(9600, SERIAL_8N1, RX, TX);
 }
 
 void loop()
